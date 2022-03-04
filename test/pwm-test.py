cda16661b2725359f6e80047a833ec6c20158919
Katleiah Ramos <katleiahramos@gmail.com>
2022-03-03 20:57:57 -0800

    update pwm test

--- test/pwm-test.py
+++ test/pwm-test.py
@@ -12,18 +12,34 @@ GPIO.setup(led_pin, GPIO.OUT)
 
 # PWM
 
-pi_pwm = GPIO.PWM(led_pin,1000)		#create PWM instance with frequency
+pi_pwm = GPIO.PWM(led_pin,50)		#create PWM instance with frequency
 pi_pwm.start(0)	
 
 # Start high
-pi_pwm.ChangeDutyCycle(100)
-time.sleep(1)
+pi_pwm.ChangeDutyCycle(2)
+print('2')
+time.sleep(.5)
+pi_pwm.ChangeDutyCycle(0)
 
+
+time.sleep(3)
+
+pi_pwm.ChangeDutyCycle(7)
+print('7')
+time.sleep(1)
+pi_pwm.ChangeDutyCycle(0)
+
+time.sleep(3)
 # Get half dim
-pi_pwm.ChangeDutyCycle(50)
+pi_pwm.ChangeDutyCycle(12)
+print('10')
 time.sleep(1)
+pi_pwm.ChangeDutyCycle(0)
+# # Get very dim
+# pi_pwm.ChangeDutyCycle(12)
+# time.sleep(1)
 
-# Get very dim
-pi_pwm.ChangeDutyCycle(25)
-time.sleep(1)
+time.sleep(3)
 
+pi_pwm.stop()
+GPIO.cleanup()
